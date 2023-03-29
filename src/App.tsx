94cfe5051db3ac8e6b4481e8451bc109ddd93ca0
Carmen Owen <doctorsout@yahoo.com>
2023-03-28 20:54:03 -0400

    Enable routing and add deployment script

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Nasa from './Components/Routes/Nasa';
+import Cards from './Components/Routes/Cards';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router basename="/playwright-ui">
+        <Routes>
+          <Route path="/" element={<h1>🛠 Under construction</h1>} />
+          <Route path="nasa" element={<Nasa />} />
+          <Route path="cards" element={<Cards />} />
+        </Routes>
+        <div className="list">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="nasa">NASA</Link></li>
+            <li><Link to="cards">Cards</Link></li>
+          </ul>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Routes/Nasa.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nasa() {
+    return (
+        <div>
+            <h1>NASA</h1>
+        </div>
+    )
+}

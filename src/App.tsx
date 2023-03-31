579379cf545946c7c7ec9ef3576e628780a3e1bf
Carmen Owen <doctorsout@yahoo.com>
2023-03-30 21:43:35 -0400

    Add starter NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Nasa from './Components/Routes/Nasa'
 import Cards from './Components/Routes/Cards'
+import NavBar from './Components/NavBar'
 
 function App() {
 	return (
 		<div className="App">
 			<Router basename="/qa-playground">
+				<NavBar />
 				<Routes>
 					<Route path="/" element={<h1>🛠 Under construction</h1>} />
 					<Route path="nasa" element={<Nasa />} />
 					<Route path="cards" element={<Cards />} />
 				</Routes>
-				<div className="list">
-					<ul>
-						<li><Link to="/">Home</Link></li>
-						<li><Link to="nasa">NASA</Link></li>
-						<li><Link to="cards">Cards</Link></li>
-					</ul>
-				</div>
 			</Router>
 		</div>
 	)
